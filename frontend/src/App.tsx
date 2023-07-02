5945ac9a2a1b351b1a4708bdf9245486f7e9b357
syorito-hatsuki <enderdron3@gmail.com>
2023-07-03 01:49:12 +0300

    Added React Route and React Query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import './App.css';
+import {Container} from "@mui/material";
+import TitleAppBar from "./components/TitleAppBar";
+import Copyright from "./components/Copyright";
+import {Outlet} from "react-router-dom";
 
 export default function App() {
     return (
         <>
-
+            <TitleAppBar/>
+            <Container maxWidth="xl">
+                <Outlet/>
+            </Container>
+            <Copyright/>
         </>
-    );
+    )
 }
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import CountriesPage from "./pages/CountriesPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 ).render(
     <React.StrictMode>
-        <App/>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={
+                createBrowserRouter(
+                    createRoutesFromElements(
+                        <Route path="/" element={<App/>}>
+                            <Route index element={<CountriesPage/>}/>
+
+                            <Route path="*" element={<NotFoundPage/>}/>
+                        </Route>
+                    )
+                )
+            }/>
+        </QueryClientProvider>
     </React.StrictMode>
 );
