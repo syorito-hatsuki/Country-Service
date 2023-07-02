56a54a9d6d4a8ec177281c70fc6b1ab6d298f0ae
syorito-hatsuki <enderdron3@gmail.com>
2023-07-03 01:46:24 +0300

    Added CORS for Spring Boot

--- backend/src/main/java/dev/syorito_hatsuki/country_service/CountryServiceApplication.java
+++ backend/src/main/java/dev/syorito_hatsuki/country_service/CountryServiceApplication.java
@@ -2,10 +2,24 @@ package dev.syorito_hatsuki.country_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CountryServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CountryServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
