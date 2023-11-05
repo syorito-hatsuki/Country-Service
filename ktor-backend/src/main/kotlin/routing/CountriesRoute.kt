bbe5ad336dfe5ee7868f2826354e92df2e144ed7
syorito-hatsuki <enderdron3@gmail.com>
2023-11-05 12:33:47 +0200

    Replaced dump data with real

--- ktor-backend/src/main/kotlin/routing/CountriesRoute.kt
+++ ktor-backend/src/main/kotlin/routing/CountriesRoute.kt
@@ -1,29 +1,31 @@
 package routing
 
-import dto.Countries
-import dto.Country
+import api.CountriesNowAPI
+import dto.CountryInfo
+import dto.CountryName
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.util.*
 
 fun Route.countriesRoute() {
     route("countries") {
         get {
-            call.respond(
-                Countries(
-                    emptyList()
-                )
-            )
+            call.respond(CountriesNowAPI.getCountries().data.map {
+                CountryName(it.name, it.iso2)
+            })
         }
 
         get("{name}") {
+            val name = call.parameters.getOrFail("name")
+            val api = CountriesNowAPI
             call.respond(
-                Country(
-                    "",
-                    "",
-                    "",
-                    0,
-                    ""
+                CountryInfo(
+                    name,
+                    api.getCountryCode(name).data.iso2,
+                    api.getCapital(name).data.capital,
+                    api.getPopulation(name).data.populationCounts.last().value,
+                    api.getFlag(name).data.flag
                 )
             )
         }
